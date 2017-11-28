425cbbd8cc3446220bb615f1b2eb0894ccebe2cd
Erick Asto Oblitas <erickastooblitas@gmail.com>
2017-11-28 07:11:48 -0500

    ->Simple Network initial code

---
+++ src/v0/IntroductionNeuralNetwork/SimpleNetwork.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def sigmoid(x):
+    # TODO: Implement sigmoid function
+    pass
+
+inputs = np.array([0.7, -0.3])
+weights = np.array([0.1, 0.8])
+bias = -0.1
+
+# TODO: Calculate the output
+output = None
+
+print('Output:')
+print(output)
