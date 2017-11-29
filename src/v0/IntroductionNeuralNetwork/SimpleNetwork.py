991a56141129e6e4564794673fe843930f5d088d
Erick Asto Oblitas <erickastooblitas@gmail.com>
2017-11-29 06:10:11 -0500

    Sigmoid Function example

--- src/v0/IntroductionNeuralNetwork/SimpleNetwork.py
+++ src/v0/IntroductionNeuralNetwork/SimpleNetwork.py
@@ -2,14 +2,14 @@ import numpy as np
 
 def sigmoid(x):
     # TODO: Implement sigmoid function
-    pass
+    return 1 / (1 + np.exp(-x))
 
 inputs = np.array([0.7, -0.3])
 weights = np.array([0.1, 0.8])
 bias = -0.1
 
 # TODO: Calculate the output
-output = None
+output = sigmoid(np.dot(inputs, weights) + bias)
 
 print('Output:')
 print(output)
